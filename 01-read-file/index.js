1d867b92e0980cbfe044d24ebdab73e1cfe8fbf7
Artem07199 <artemlitasov@gmail.com>
2021-11-05 16:26:28 +0300

    complite 2

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const stream = new fs.ReadStream('01-read-file/text.txt', 'utf8');
+
+
+stream.on('readable', function(){
+  const data = stream.read();
+  if (data){
+    console.log(data);
+  }
+});
---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const process = require('process');
+const way = fs.createWriteStream(path.resolve('./02-write-file/text.txt'), {flags: 'a'});
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '>'
+});
+rl.write('Please, write your text here:\n');
+rl.prompt();
+rl.on('line', line => {
+  if (line === 'exit') {
+    rl.close(); 
+  } else {
+    way.write(line + '\n');
+    console.log('More: ');
+    rl.prompt();
+  }
+});
+process.on('exit', () => {
+  console.log('Goodbye');
+});
+
