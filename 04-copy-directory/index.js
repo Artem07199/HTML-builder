7b4f33e895476508599dd44f1391f49cb53aef82
Artem07199 <artemlitasov@gmail.com>
2021-11-07 15:36:37 +0300

    add 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+
+
+fs.mkdir('./04-copy-directory/files-copy', { recursive: true }, () => {
+  fs.readdir('./04-copy-directory/files', (err, data) => {
+    data.forEach(file => {
+      fs.copyFile('./04-copy-directory/files' + '/' + file, './04-copy-directory/files-copy' + '/' + file, function() {
+      });
+    });
+    console.log('File Copied');
+  });
+      
+  fs.readdir('./04-copy-directory/files-copy', (err, data) => {
+    data.forEach(file => {
+      fs.stat('./04-copy-directory/files' + '/' + file, function ( err) {
+        if (err) {
+          fs.unlink('./04-copy-directory/files-copy' + '/' + file, function () {});
+        }
+      });
+    });
+  });
+
+});
+
 
